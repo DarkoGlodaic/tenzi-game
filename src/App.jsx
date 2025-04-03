fbc1e598363e5b44fda9569c27d717e0f61b376c
Darko Glodaic <darko.glodjaic@gmail.com>
2025-04-03 16:04:39 +0200

    Created Die component and added row styling to Dice div.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,26 @@ import './App.css'
 
 function App() {
   return (
-    <main></main>
+    <main>
+      <div className='dice-container'>
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+        <Die value={5}/>
+        <Die value={6}/>
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+      </div>
+    </main>
+  )
+}
+
+function Die(props) {
+  return (
+    <button>{props.value}</button>
   )
 }
 
