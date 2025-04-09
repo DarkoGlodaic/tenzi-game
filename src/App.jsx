94afb7903940820b727a09e533f5a6c1052c66bd
Darko Glodaic <darko.glodjaic@gmail.com>
2025-04-09 13:08:33 +0200

    Added Roll! button that re-rolls all dice.

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,21 @@ import Die from './components/Die'
 function App() {
   const [diceValue, setDiceValue] = useState(generateAllNewDice());
   const diceElements = diceValue.map(num => <Die value={num}/>)
+
+  const rollDice = () => {
+    setDiceValue(prevDice => {
+      // const newDice = [...prevDice]
+      return generateAllNewDice()
+    })
+  }
+
   return (
     <main>
       <div className='dice-container'>
         {diceElements}
       </div>
+
+      <button className="roll-dice" onClick={rollDice}>Roll!</button>
     </main>
   )
 }
